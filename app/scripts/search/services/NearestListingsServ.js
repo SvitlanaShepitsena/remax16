318fba422e25daf62892ed69dd34258d042c1129
AndriyShepitsen <chicagobusinessintelligence2@gmail.com>
2015-10-29 13:59:10 -0500

    Add files

---
+++ app/scripts/search/services/NearestListingsServ.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+    angular.module('search')
+        .factory('NearestListingsServ', function ($q, url, users, $firebaseObject, $firebaseArray) {
+            return {
+                get: function () {
+                },
+                get: function () {
+                    return $q(function (resolve, reject) {
+                    });
+                }
+            };
+        });
+})();

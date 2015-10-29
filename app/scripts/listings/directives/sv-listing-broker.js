cf8d30fd10716606eb3a07b1985b997c02848cfe
SvitlanaShepitsena <chicagobusinessintelligence1@gmail.com>
2015-10-29 11:17:49 -0500

    Header top nav design #18

--- app/scripts/listings/directives/sv-listing-broker.js
+++ app/scripts/listings/directives/sv-listing-broker.js
@@ -1,12 +1,13 @@
 (function () {
-	'use strict';
-	angular.module('listings')
-		.directive('svListingBroker', function (userAuth) {
-			return {
-				replace: true,
-				templateUrl: 'scripts/listings/directives/sv-listing-broker.html',
-				link: function ($scope, el, attrs) {
-				}
-			};
-		});
+    'use strict';
+    angular.module('listings')
+        .directive('svListingBroker', function (userAuth, companyPhone) {
+            return {
+                replace: true,
+                templateUrl: 'scripts/listings/directives/sv-listing-broker.html',
+                link: function ($scope, el, attrs) {
+                    $scope.companyPhone = companyPhone;
+                }
+            };
+        });
 })();

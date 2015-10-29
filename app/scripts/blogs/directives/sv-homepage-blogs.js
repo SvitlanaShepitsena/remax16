cd6f5aa593e37f030787e9e19f343a60095c2232
SvitlanaShepitsena <chicagobusinessintelligence1@gmail.com>
2015-10-29 14:31:03 -0500

    Home page navigation for blogs #12

--- app/scripts/blogs/directives/sv-homepage-blogs.js
+++ app/scripts/blogs/directives/sv-homepage-blogs.js
@@ -2,13 +2,12 @@
     'use strict';
 
     angular.module('blogs')
-        .directive('svHomepageBlogs', function (BlogsServ,$sce) {
+        .directive('svHomepageBlogs', function (BlogsServ, $sce) {
             return {
                 replace: true,
                 templateUrl: 'scripts/blogs/directives/sv-homepage-blogs.html',
                 scope: {},
                 link: function ($scope, el, attrs) {
-
                     BlogsServ.getRandomBlogs().then(function (blogs) {
                         $scope.blogs = blogs;
 

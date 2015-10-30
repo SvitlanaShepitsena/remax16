3a7dd6ffa1d51e33eacb691c6e86cd2764f42e4b
SvitlanaShepitsena <chicagobusinessintelligence1@gmail.com>
2015-10-30 16:23:03 -0500

    Add files

--- app/scripts/customers/customersRoutes.js
+++ app/scripts/customers/customersRoutes.js
@@ -2,16 +2,19 @@
     'use strict'
 
     angular.module('customers', [])
-    .config(function ($stateProvider, $urlRouterProvider) {
-        $stateProvider
-
-				
-				.state("app.customers", {
-					url: "/customers", 
-					controller:"CustomersCtrl",
-					templateUrl: "scripts/customers/views/customersCtrl.html"
+        .config(function ($stateProvider, $urlRouterProvider) {
+            $stateProvider
+                .state("app.customers", {
+                    url: "/customers",
+                    controller: "CustomersCtrl",
+                    templateUrl: "scripts/customers/views/customersCtrl.html"
+                })
+				.state("app.one-customer", {
+					url: "/one-customer", 
+					controller:"OneCustomerCtrl",
+					templateUrl: "scripts/customers/views/one-customerCtrl.html"
 				})
 //#state'
 
-    });
+        });
 })();

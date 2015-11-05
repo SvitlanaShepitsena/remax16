d5245560404890349b8eea5d3239806b1f2ad319
SvitlanaShepitsena <chicagobusinessintelligence1@gmail.com>
2015-11-05 12:34:03 -0600

    Add files

---
+++ app/scripts/common/services/VMServ.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+    angular.module('common')
+        .factory('VMServ', function ($q, url, users, $firebaseObject, $firebaseArray) {
+            return {
+                get: function () {
+                },
+                get: function () {
+                    return $q(function (resolve, reject) {
+                    });
+                }
+            };
+        });
+})();

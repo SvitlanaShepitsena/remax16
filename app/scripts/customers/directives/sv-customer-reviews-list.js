8c3ff159e4db655f8c44e3a62376347faa14c798
SvitlanaShepitsena <chicagobusinessintelligence1@gmail.com>
2015-11-05 12:22:34 -0600

    Add files

---
+++ app/scripts/customers/directives/sv-customer-reviews-list.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    angular.module('customers')
+        .directive('svCustomerReviewsList', function () {
+            return {
+                replace: true,
+                templateUrl: 'scripts/customers/directives/sv-customer-reviews-list.html',
+                scope: {
+
+                },
+                link: function ($scope, el, attrs) {
+
+                }
+            };
+        });
+})();

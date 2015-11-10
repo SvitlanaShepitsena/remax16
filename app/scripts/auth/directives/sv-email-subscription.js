0792fe0034ac57285ebb643d77f8c590b23627e2
AndriyShepitsen <chicagobusinessintelligence2@gmail.com>
2015-11-10 09:35:06 -0600

    Subscriptions

--- app/scripts/auth/directives/sv-email-subscription.js
+++ app/scripts/auth/directives/sv-email-subscription.js
@@ -1,22 +1,15 @@
 (function () {
     'use strict';
     angular.module('auth')
-        .directive('svEmailSubscription', function (PdfSubscriptionsServ, userAuth) {
+        .directive('svEmailSubscription', function (userAuth,FbGenServ) {
             return {
                 replace: true,
                 templateUrl: 'scripts/auth/directives/sv-email-subscription.html',
                 scope: {},
                 link: function ($scope) {
                     $scope.user = userAuth.profile;
-                    var pdfSubObj = PdfSubscriptionsServ.getObjectRef();
-                    if (pdfSubObj) {
-                        $scope.requestSubmited = true;
-                        pdfSubObj.$bindTo($scope, 'subscription').then(function () {
-                            $scope.requestSubmited = false;
-                        }).catch(function (error) {
-                            $scope.requestSubmited = false;
-                        });
-                    }
+
+
                 }
             };
         });

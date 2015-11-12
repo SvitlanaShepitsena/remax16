ca5a781ad743ff3d9e0cd3bde8ed4f5ebd265f35
AndriyShepitsen <chicagobusinessintelligence2@gmail.com>
2015-11-12 15:30:34 -0600

    add files

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -54,14 +54,6 @@
         })
         .config(function ($mdThemingProvider, $mdIconProvider, $compileProvider) {
             $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|mms):/);
-            $mdIconProvider
-                .defaultIconSet("./assets/svg/avatars.svg", 128)
-                .icon("menu", "./assets/svg/menu.svg", 24)
-                .icon("share", "./assets/svg/share.svg", 24)
-                .icon("google_plus", "./assets/svg/google_plus.svg", 512)
-                .icon("hangouts", "./assets/svg/hangouts.svg", 512)
-                .icon("twitter", "./assets/svg/twitter.svg", 512)
-                .icon("phone", "./assets/svg/phone.svg", 512);
             $mdThemingProvider.theme('default')
                 .primaryPalette('blue')
                 .accentPalette('red');

16db953195a83aa542f9e4f6b26c8b47165b35d0
AndriyShepitsen <chicagobusinessintelligence2@gmail.com>
2015-11-15 16:25:39 -0600

    add files

--- services/HomepageRenderServ.js
+++ services/HomepageRenderServ.js
@@ -3,35 +3,9 @@ var _ = require('lodash');
 var tagsArticleFilter = require('../filters/TagsArticlesFilter');
 var appconstants = require('./const');
 module.exports = {
-    process: function (news) {
-        var articles = _.filter(news, function (oneNews) {
-            return oneNews.isBlog === false;
-        });
-        var blogs = _.filter(news, function (oneNews) {
-            return oneNews.isBlog === true;
-        });
-        var endorsedBySvet = tagsArticleFilter.getArticleWithTags(articles, appconstants.endorsedSvetTags)
-        var svetOnly = tagsArticleFilter.getArticleWithTags(news, appconstants.onlySvetTags)
-        var personality = tagsArticleFilter.getArticleWithTags(news, appconstants.personalityTags)
-        articles = _.sortBy(articles, function (article) {
-            return article.newsOrder;
-        })
-        var mainNews = _.first(articles);
-        var secondNews = articles[1];
-        var thirdNews = articles[2];
-        var ordinalArticles = articles.slice(3);
-        var blog = _.first(_.sortBy(blogs, function (blog) {
-            return -blog.timestamp;
-        }));
-        return {
-            mainNews: mainNews,
-            secondNews: secondNews,
-            thirdNews: thirdNews,
-            ordinalArticles: ordinalArticles,
-            blog: blog,
-            endorsedSvet: endorsedBySvet,
-            svetOnly: svetOnly,
-            personality: personality
-        }
+    process: function (blogs) {
+        var sortBlogs = _.sortBy(blogs, 'timestamp');
+        sortBlogs = _.take(sortBlogs, 4);
+        return sortBlogs;
     }
 }

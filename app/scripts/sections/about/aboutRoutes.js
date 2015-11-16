508f7aba012800519263c0e2fd0e56f0fdee7373
SvitlanaShepitsena <chicagobusinessintelligence1@gmail.com>
2015-11-16 00:10:48 -0600

    About Section refactoring

--- app/scripts/sections/about/aboutRoutes.js
+++ app/scripts/sections/about/aboutRoutes.js
@@ -10,9 +10,5 @@
                     controller: "AboutCtrl",
                     templateUrl: "scripts/sections/about/views/aboutCtrl.html"
                 })
-                .state("app.about.google-analytics", {
-                    url: "/google-analytics",
-                    templateUrl: "scripts/sections/about/views/google-analyticsCtrl.html"
-                })
         });
 })();

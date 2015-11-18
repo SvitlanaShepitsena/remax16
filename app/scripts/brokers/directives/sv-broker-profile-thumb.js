c7043d3741dc58cbe176e9e00b0d91a206e9f739
SvitlanaShepitsena <chicagobusinessintelligence1@gmail.com>
2015-11-18 15:37:30 -0600

    Design for default phone on broker profile #103

--- app/scripts/brokers/directives/sv-broker-profile-thumb.js
+++ app/scripts/brokers/directives/sv-broker-profile-thumb.js
@@ -9,7 +9,15 @@
                 link: function ($scope, el, attrs) {
                     $scope.avatarBroker = avatarBroker;
                     $scope.companyPhone = companyPhone;
+                    var tel = $scope.agent.defaultTel;
+                    var phoneParameter = tel ? 'phone' + tel : null;
+                    if (phoneParameter) {
+                        $scope.contactPhone = $scope.agent[phoneParameter];
 
+                    } else {
+                        $scope.contactPhone = companyPhone;
+
+                    }
                 }
             };
         });

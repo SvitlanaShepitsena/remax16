f2e9693e37e1a67d105b24a5e802f6e63acc25b8
SvitlanaShepitsena <chicagobusinessintelligence1@gmail.com>
2015-11-18 23:12:50 -0600

    Broker profile functionality

--- app/scripts/brokers/directives/sv-default-phone.js
+++ app/scripts/brokers/directives/sv-default-phone.js
@@ -2,19 +2,19 @@
     'use strict';
 
     angular.module('brokers')
-        .directive('svDefaultPhone', function (userAuth, url, FbGenServ, $stateParams) {
+        .directive('svDefaultPhone', function (userAuth, url, FbGenServ, $stateParams, companyFax) {
             return {
                 replace: true,
                 templateUrl: 'scripts/brokers/directives/sv-default-phone.html',
                 scope: {
                     broker: '=',
-                    editRight:'='
+                    editRight: '='
                 },
                 link: function ($scope, el, attrs) {
                     var brokerId = $stateParams.id;
+                    $scope.companyFax = companyFax;
 
                     var phones = [{name: 'Office', value: '(847) 674-9797'}];
-
                     _.forOwn($scope.broker, function (val, prop) {
                         if (prop.indexOf('phone') > -1) {
                             var phName = _.snakeCase(prop).split('_')[1];
@@ -23,7 +23,6 @@
                                 value: val
                             });
                         }
-
                     });
                     $scope.phones = phones;
 
@@ -31,8 +30,6 @@
                         var brokerUrl = url + 'homes/agents/' + $scope.broker.$id;
                         FbGenServ.saveObject(brokerUrl, {defaultTel: phone.name});
                     };
-
-
                 }
             };
         });

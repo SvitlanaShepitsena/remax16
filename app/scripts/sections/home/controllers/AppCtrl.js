8a30556d8e9d9c49517f9ce8daf22b5c62220a46
SvitlanaShepitsena <chicagobusinessintelligence1@gmail.com>
2015-11-23 11:28:07 -0600

    Desktop footer and header info #108

--- app/scripts/sections/home/controllers/AppCtrl.js
+++ app/scripts/sections/home/controllers/AppCtrl.js
@@ -1,60 +1,64 @@
 (function () {
     'use strict';
     angular.module('sections.home')
-        .controller('AppCtrl', function AppCtrl($sce, AgentServ, NotificationsServ, userAuth, appNews, $timeout, $mdSidenav, $mdMedia,
-                                                 $state, $scope, $rootScope, toastr) {
-            $rootScope.frameUrl = $sce.trustAsResourceUrl("http://localhost");
-            $rootScope.user = userAuth.profile;
-            $rootScope.news = appNews.public;
+        .controller('AppCtrl', function AppCtrl($sce, AgentServ, NotificationsServ, userAuth, appNews, $timeout, $mdSidenav, $mdMedia, companyPhone,
+                                                $state, $scope, $rootScope, toastr) {
+                $scope.companyPhone = companyPhone;
 
-            $rootScope.$on('homeNewsChanged', function () {
+                $rootScope.frameUrl = $sce.trustAsResourceUrl("http://localhost");
+                $rootScope.user = userAuth.profile;
                 $rootScope.news = appNews.public;
-            });
-            var currentRoute = $state.current;
-            if (currentRoute.url === '/') {
-                $rootScope.homeStyle = true;
-            } else {
-                $rootScope.homeStyle = false;
-            }
-            $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams, error) {
-                var routeName = toState.name;
-                if (_.contains('app.home', routeName)) {
+
+                $rootScope.$on('homeNewsChanged', function () {
+                    $rootScope.news = appNews.public;
+                });
+                var currentRoute = $state.current;
+                if (currentRoute.url === '/') {
                     $rootScope.homeStyle = true;
                 } else {
                     $rootScope.homeStyle = false;
                 }
-            });
-            $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
-                toastr.warning(error);
-            });
-            $rootScope.$on('error', function () {
-                toastr.error('error');
-            });
-            $scope.$on('image-search-show', function () {
-                $scope.imageSearch = true;
-            });
-            $scope.$on('image-search-hide', function () {
-                $scope.imageSearch = false;
-            });
+                $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams, error) {
+                    var routeName = toState.name;
+                    if (_.contains('app.home', routeName)) {
+                        $rootScope.homeStyle = true;
+                    } else {
+                        $rootScope.homeStyle = false;
+                    }
+                });
+                $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+                    toastr.warning(error);
+                });
+                $rootScope.$on('error', function () {
+                    toastr.error('error');
+                });
+                $scope.$on('image-search-show', function () {
+                    $scope.imageSearch = true;
+                });
+                $scope.$on('image-search-hide', function () {
+                    $scope.imageSearch = false;
+                });
 
-            $rootScope.toggleRight = function () {
-                $mdSidenav('right').toggle();
-            };
-            $rootScope.toggleLeft = function () {
-                $mdSidenav('left').toggle();
-            };
-            $rootScope.$watch('appLoaded', function (newValue) {
-                $scope.appLoaded = newValue;
-            });
-            $scope.$watch(function () {
-                return $mdMedia('gt-lg');
-            }, function (rightPanelShown) {
-                if (rightPanelShown) {
-                    $scope.showShifter = !AgentServ.isIe();
-                } else {
-                    $scope.showShifter = true;
-                }
-            });
-        });
+                $rootScope.toggleRight = function () {
+                    $mdSidenav('right').toggle();
+                };
+                $rootScope.toggleLeft = function () {
+                    $mdSidenav('left').toggle();
+                };
+                $rootScope.$watch('appLoaded', function (newValue) {
+                    $scope.appLoaded = newValue;
+                });
+                $scope.$watch(function () {
+                    return $mdMedia('gt-lg');
+                }, function (rightPanelShown) {
+                    if (rightPanelShown) {
+                        $scope.showShifter = !AgentServ.isIe();
+                    } else {
+                        $scope.showShifter = true;
+                    }
+                });
+            }
+        )
+    ;
 })();
 
